96f7e83e5bb2d9266d93650de72f3b60a4e0037f
Brandon Felty <brcfelty@gmail.com>
2022-04-14 21:46:33 -0600

    created new ISS spotter that uses promises instead of nested callbacks

---
+++ iss_promised.js
@@ -0,0 +1,38 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ip-api.com/json/${ip}`);
+}
+
+const fetchISSFlyOverTimes = function(body) {
+  const { lat, lon } = JSON.parse(body);
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${lat}&lon=${lon}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+module.exports = { nextISSTimesForMyLocation, printPassTimes };
